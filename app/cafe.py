2983b56c2c036f4a564f477fec08ba0b8db41b43
Yurnerroo <yura.zdanevych@gmail.com>
2023-08-18 11:13:43 +0300

    code refactoring

--- app/cafe.py
+++ app/cafe.py
@@ -1,19 +1,22 @@
-from app.errors import NotVaccinatedError, \
-                        OutdatedVaccineError, \
-                        NotWearingMaskError
 import datetime
+
+from app.errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError,
+)
 
 
 class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, vstr: dict) -> str:
-        if "vaccine" not in vstr.keys() or vstr["vaccine"] == None:
+    def visit_cafe(self, visitor: dict) -> str:
+        if not visitor.get("vaccine"):
             raise NotVaccinatedError("not vaccinated")
-        elif vstr["vaccine"]["expiration_date"] < datetime.date.today():
+        elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError("outdated")
-        elif not vstr["wearing_a_mask"]:
+        elif not visitor["wearing_a_mask"]:
             raise NotWearingMaskError("not wearing mask")
         else:
-            return "Welcome to " + self.name
+            return f"Welcome to {self.name}"
